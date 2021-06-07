214703760ad1f74900c62fc8fc4804370cede100
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-06 21:00:50 -0400

    Update app.java

--- src/main/java/base/app.java
+++ src/main/java/base/app.java
@@ -1,3 +1,8 @@
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Korinne Ramcharitar
+ */
 package base;
 //Division isn’t always exact, and sometimes you’ll write programs that will need to deal with the leftovers as a whole number instead of a decimal.
 //
